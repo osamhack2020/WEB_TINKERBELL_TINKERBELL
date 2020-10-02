02702b76aa1387252be3bb85c5d9a19ceb9602f9
mjh4ram <mjh4ram@gmail.com>
2020-10-02 13:19:38 +0900

    auth

--- backend/views.py
+++ backend/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+# the post request is protected by cors-oprigin-whitelist
+# the rquest origin is a React App that passes clean user inputs only
 @csrf_exempt
 def register(request):
     if request.method == "POST":
@@ -13,12 +15,24 @@ def register(request):
         password = data["password"]
         # check if username exists
         if User.objects.filter(username=username).exists():
-            print(1)
             return HttpResponse(status=406)
         else:
             User.objects.create_user(username=username, password=password)
-            user = authenticate(username=username, password=password)
-            login(request, user)
+            print("ok")
             return HttpResponse(status=200)
+    else:
+        return HttpResponse(status=404)
+
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        username = data["username"]
+        password = data["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            return HttpREsponse(status=200)
+        else:
+            return HttpResponse(status=406)
     else:
         return HttpResponse(status=404)
--- frontend/src/components/signup.js
+++ frontend/src/components/signup.js
@@ -1,36 +1,59 @@
 import React, { Component } from 'react';
 import '../css/signup.css';
+import UserStore from './userstore';
+import { Redirect } from 'react-router-dom';
+
 
 class Signup extends Component {
   constructor(props) {
     super(props);
-    this.state = { username: '', password1: '', password2: '', match: false};
+    this.state = { username: '', password1: '', password2: '', match: false, redirect: false};
   }
 
-  sendSigninRequest() {
+  async sendSigninRequest() {
     const requestOptions = {
       method: 'POST',
       headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-      body: JSON.stringify({ username: this.state.username, password: this.state.password1})
+      body: JSON.stringify({ username: this.state.username, password: this.state.password1 })
     };
-    fetch('http://localhost:8000/signup/', requestOptions).then((response) => {
-      if(response.ok) console.log("OK");
-      else console.log("failed");
-    });
+
+    UserStore.loading = true;
+    const response = await fetch('http://localhost:8000/signup/', requestOptions);
+    console.log("this?");
+    if (response.ok) {
+      console.log("123123");
+      UserStore.isLoggedIn = true;
+      UserStore.username = this.state.username;
+      UserStore.loading = false;
+      this.context.router.push('/anotherroute');
+    }else {
+      console.log("asjklflksahflkashf");
+      UserStore.loading = false;
+      UserStore.isLoggedIn = true;
+    }
   }
 
-
     render() {
-        return (
-          <div class="Sigup">
-            <form>
-              <input type="text" placeholder="Enter Username" onChange={(e) => this.setState({username: e.target.value})} required/>
-              <input type="password" placeholder="Enter Password" onChange={(e) => this.setState({password1: e.target.value})} required/>
-              <input type="password2" placeholder="Enter Password Again" onChange={(e) => this.setState({password2: e.target.value})} required/>
-              <button type="submit" onClick={()=> this.sendSigninRequest()}>회원가입</button>
-            </form>
-          </div>
-        )
+        if (UserStore.loading) {
+          return <div>loading~</div>
+        }
+        else if (UserStore.isLoggedIn) {
+          return <Redirect to ='/'/>
+        }
+        else{
+          return (
+            <div class="Signup">
+              <form>
+                <input type="text" placeholder="Enter Username" onChange={(e) => this.setState({username: e.target.value})} required/>
+                <input type="password" placeholder="Enter Password" onChange={(e) => this.setState({password1: e.target.value})} required/>
+                <input type="password2" placeholder="Enter Password Again" onChange={(e) => this.setState({password2: e.target.value})} required/>
+                <button type="submit" onClick={()=> this.sendSigninRequest()}>회원가입</button>
+              </form>
+            </div>
+          )
+        }
+
+
     }
 }
 
