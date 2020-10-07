a7ff3c94d9c4b9e64e2aeaea5d766d5f20dc4f47
mjh4ram <mjh4ram@gmail.com>
2020-10-07 23:33:07 +0900

    about component 업데이트

--- frontend/src/components/about.js
+++ frontend/src/components/about.js
@@ -4,7 +4,70 @@ import '../css/about.css';
 class About extends Component {
     render() {
         return (
-            <div class="about"></div>
+            <div class="about">
+              <div class="cards">
+                <div class="card-space card">
+                  <svg width="73px" height="60px" viewBox="0 0 73 60" version="1.1">
+                      <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="Group-5" transform="translate(-49.000000, -69.000000)" stroke="white" stroke-width="1.5">
+                              <g id="30">
+                                  <g transform="translate(50.000000, 70.000000)">
+                                      <rect id="Rectangle" x="0.449367089" y="0.453125" width="69.8496203" height="51.3209375"></rect>
+                                      <polyline id="Path" points="46.0241772 43.2915625 59.7029114 29.4984375 46.0241772 15.7053125"></polyline>
+                                      <polyline id="Path" points="24.7151899 15.7053125 11.0364557 29.4984375 24.7151899 43.2915625"></polyline>
+                                      <path d="M39.9397468,6.5975 L30.9434177,51.7740625" id="Path"></path>
+                                      <rect id="Rectangle" x="0.449367089" y="6.5975" width="69.8496203" height="51.3209375"></rect>
+                                  </g>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                  <h1>높은 정확도</h1>
+                  <p>정확도가 많이많이 많이 많이 많이 많이 많이 많이많이 많이 많이많이 많이 많이많이 많이 많이높습니다</p>
+                  <div class="learn-more">Learn More </div>
+                </div>
+                <div class="card-space card">
+                  <svg width="73px" height="60px" viewBox="0 0 73 60" version="1.1">
+                      <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="Group-5" transform="translate(-49.000000, -69.000000)" stroke="white" stroke-width="1.5">
+                              <g id="30">
+                                  <g transform="translate(50.000000, 70.000000)">
+                                      <rect id="Rectangle" x="0.449367089" y="0.453125" width="69.8496203" height="51.3209375"></rect>
+                                      <polyline id="Path" points="46.0241772 43.2915625 59.7029114 29.4984375 46.0241772 15.7053125"></polyline>
+                                      <polyline id="Path" points="24.7151899 15.7053125 11.0364557 29.4984375 24.7151899 43.2915625"></polyline>
+                                      <path d="M39.9397468,6.5975 L30.9434177,51.7740625" id="Path"></path>
+                                      <rect id="Rectangle" x="0.449367089" y="6.5975" width="69.8496203" height="51.3209375"></rect>
+                                  </g>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                  <h1>높은 정확도</h1>
+                  <p>정확도가 많이많이 많이 많이 많이 많이 많이 많이많이 많이 많이많이 많이 많이많이 많이 많이높습니다</p>
+                  <div class="learn-more">Learn More </div>
+                </div>
+                <div class="card">
+                  <svg width="73px" height="60px" viewBox="0 0 73 60" version="1.1">
+                      <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="Group-5" transform="translate(-49.000000, -69.000000)" stroke="white" stroke-width="1.5">
+                              <g id="30">
+                                  <g transform="translate(50.000000, 70.000000)">
+                                      <rect id="Rectangle" x="0.449367089" y="0.453125" width="69.8496203" height="51.3209375"></rect>
+                                      <polyline id="Path" points="46.0241772 43.2915625 59.7029114 29.4984375 46.0241772 15.7053125"></polyline>
+                                      <polyline id="Path" points="24.7151899 15.7053125 11.0364557 29.4984375 24.7151899 43.2915625"></polyline>
+                                      <path d="M39.9397468,6.5975 L30.9434177,51.7740625" id="Path"></path>
+                                      <rect id="Rectangle" x="0.449367089" y="6.5975" width="69.8496203" height="51.3209375"></rect>
+                                  </g>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+                  <h1>높은 정확도</h1>
+                  <p>정확도가 많이많이 많이 많이 많이 많이 많이 많이많이 많이 많이많이 많이 많이많이 많이 많이높습니다</p>
+                  <div class="learn-more">Learn More </div>
+                </div>
+              </div>
+            </div>
         )
     }
 }
