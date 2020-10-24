9230f7a9ad141cebbc8c6b936c848d6778a0caeb
m <mjh4ram@gmail.com>
2020-10-24 22:17:15 +0900

    about.js class에서 function component로변경

--- frontend/src/components/about.js
+++ frontend/src/components/about.js
@@ -1,75 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Card from './card';
 import '../css/about.css';
 
-class About extends Component {
-    render() {
-        return (
-            <div class="about">
-              <div class="cards">
-                <div class="card-space card">
-                  <svg width="73px" height="60px" viewBox="0 0 73 60" version="1.1">
-                      <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                          <g id="Group-5" transform="translate(-49.000000, -69.000000)" stroke="#470166" stroke-width="1.5">
-                              <g id="30">
-                                  <g transform="translate(50.000000, 70.000000)">
-                                      <rect id="Rectangle" x="0.449367089" y="0.453125" width="69.8496203" height="51.3209375"></rect>
-                                      <polyline id="Path" points="46.0241772 43.2915625 59.7029114 29.4984375 46.0241772 15.7053125"></polyline>
-                                      <polyline id="Path" points="24.7151899 15.7053125 11.0364557 29.4984375 24.7151899 43.2915625"></polyline>
-                                      <path d="M39.9397468,6.5975 L30.9434177,51.7740625" id="Path"></path>
-                                      <rect id="Rectangle" x="0.449367089" y="6.5975" width="69.8496203" height="51.3209375"></rect>
-                                  </g>
-                              </g>
-                          </g>
-                      </g>
-                  </svg>
-                  <h1>편리함</h1>
-                  <p>언제든지 접속해서 팅커벨에게 물어보세요! 언제든지 답할 준비가 되어있습니다</p>
-                  <div class="learn-more">Learn More </div>
-                </div>
-                <div class="card-space card">
-                  <svg width="73px" height="60px" viewBox="0 0 73 60" version="1.1">
-                      <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                          <g id="Group-5" transform="translate(-49.000000, -69.000000)" stroke="#470166" stroke-width="1.5">
-                              <g id="30">
-                                  <g transform="translate(50.000000, 70.000000)">
-                                      <rect id="Rectangle" x="0.449367089" y="0.453125" width="69.8496203" height="51.3209375"></rect>
-                                      <polyline id="Path" points="46.0241772 43.2915625 59.7029114 29.4984375 46.0241772 15.7053125"></polyline>
-                                      <polyline id="Path" points="24.7151899 15.7053125 11.0364557 29.4984375 24.7151899 43.2915625"></polyline>
-                                      <path d="M39.9397468,6.5975 L30.9434177,51.7740625" id="Path"></path>
-                                      <rect id="Rectangle" x="0.449367089" y="6.5975" width="69.8496203" height="51.3209375"></rect>
-                                  </g>
-                              </g>
-                          </g>
-                      </g>
-                  </svg>
-                  <h1>정확도</h1>
-                  <p>팅커벨은 최적화된 기계학습 알고리즘을 이용하여 정확하게 질문자의 의도를 파악합니다</p>
-                  <div class="learn-more">Learn More </div>
-                </div>
-                <div class="card">
-                  <svg width="73px" height="60px" viewBox="0 0 73 60" version="1.1">
-                      <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                          <g id="Group-5" transform="translate(-49.000000, -69.000000)" stroke="#470166" stroke-width="1.5">
-                              <g id="30">
-                                  <g transform="translate(50.000000, 70.000000)">
-                                      <rect id="Rectangle" x="0.449367089" y="0.453125" width="69.8496203" height="51.3209375"></rect>
-                                      <polyline id="Path" points="46.0241772 43.2915625 59.7029114 29.4984375 46.0241772 15.7053125"></polyline>
-                                      <polyline id="Path" points="24.7151899 15.7053125 11.0364557 29.4984375 24.7151899 43.2915625"></polyline>
-                                      <path d="M39.9397468,6.5975 L30.9434177,51.7740625" id="Path"></path>
-                                      <rect id="Rectangle" x="0.449367089" y="6.5975" width="69.8496203" height="51.3209375"></rect>
-                                  </g>
-                              </g>
-                          </g>
-                      </g>
-                  </svg>
-                  <h1>친근함</h1>
-                  <p>지루한 근무시간에 잠깐의 말동무가 되어드립니다 팅커벨과 친구가 되어보세요!</p>
-                  <div class="learn-more">Learn More </div>
-                </div>
-              </div>
-            </div>
-        )
-    }
+function About() {
+  return (
+      <div class="about">
+        <div class="cards">
+          <Card class_="card-space card" h1="편리함" p="언제든지 접속해서 팅커벨에게 물어보세요! 언제든지 답할 준비가 되어있습니다"/>
+          <Card class_="card-space card" h1="정확도" p="팅커벨은 최적화된 기계학습 알고리즘을 이용하여 정확하게 질문자의 의도를 파악합니다"/>
+          <Card class_="card" h1="친근함" p="지루한 근무시간에 잠깐의 말동무가 되어드립니다 팅커벨과 친구가 되어보세요!"/>
+        </div>
+      </div>
+  );
 }
 
 export default About;
